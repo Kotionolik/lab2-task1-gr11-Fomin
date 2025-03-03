94484c54b55c01a3ef9fd34e8e72984b97dffe2c
Kotionolik <146421689+Kotionolik@users.noreply.github.com>
2025-03-03 13:50:38 +0000

    Task 1 added finding prime function

--- src/main.c
+++ src/main.c
@@ -1,5 +1,26 @@
 /* hello.c */
 #include <stdio.h>
+#include <stdbool.h>
+
+void Prime()
+{
+	bool prme = true;
+	for (int i = 100; i < 1000; i++)
+	{
+			for(int j = 2; j < (i / 2) + 1; j++)
+			{
+					if(i % j == 0)
+					{
+							prme = false;
+							break;
+					}
+			}
+		if(prme)
+		{
+			printf("%d", i);
+		}
+	}
+}
 
 void main (void)
 {
@@ -7,5 +28,12 @@ void main (void)
 	for(int i = 100; i < 1000; i++)
 	{
 			printf("%d\n", i);
+	}
+	char ans;
+	printf("Do you want to see the prime numbers? (y/any key)\n");
+	scanf("%c", &ans);
+	if(ans == 'y')
+	{
+		Prime();
 	}
 }
